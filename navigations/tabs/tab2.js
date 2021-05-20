4f087294c236bb805f3471634308187cd3894373
Wsaeauiwawa <wathinee477@gmail.com>
2021-05-21 00:36:24 +0700

    update tab2&3

--- navigations/tabs/tab2.js
+++ navigations/tabs/tab2.js
@@ -1,14 +1,87 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet, TextInput } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 class tab2 extends Component {
+    onChangeText(text){
+        console.log('texChanged', text)
+        // let filterArray = this.state.filterHerb
+        // let searchResult = filterArray.filter(leaf => 
+        //   leaf.name.includes(text)
+        // )
+        // this.setState({herb: searchResult})
+
+    }
     render() {
         return (
-            <View>
-                <Text> tab2 </Text>
+            <View style={styles.container}>
+            <View style={{backgroundColor:'#c0e7cd'}}>
+              <TextInput
+                placeholder="Search"
+                style={styles.txtinput}
+                onChangeText={text => this.onChangeText(text)}
+              />
+              <Icon style={styles.icon} name="search" size={20} color="#ddd" />
             </View>
+          </View>
         )
     }
 }
 
 export default tab2;
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor:'#c0e7cd'
+    },
+  
+    txt: {
+      marginTop: 10,
+      fontSize: 16,
+      fontWeight: 'bold',
+      color: '#fff'
+    },
+    header: {
+      borderBottomWidth: 1,
+      borderColor: '#ddd',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: 50,
+    },
+    txtinput: {
+      padding: 7,
+      borderWidth: 1,
+      borderColor: '#ddd',
+      borderRadius: 5,
+      fontSize: 16,
+      margin: 15,
+      paddingLeft: 30,
+      backgroundColor: '#fff',
+    },
+  
+    icon: {
+      position: 'absolute',
+      left: 22,
+      top: 22,
+    },
+  
+    list: {
+      marginHorizontal: 10,
+      marginVertical: 5,
+      borderRadius: 10,
+      borderColor: '#ddd',
+      borderWidth: 1,
+      padding: 10,
+      backgroundColor: '#00a352'
+    },
+  
+    txtlist: {
+      fontSize: 15,
+      fontWeight: 'bold',
+      marginBottom: 5,
+      color: '#fff',
+      fontFamily: "K2D-Light"
+    },
+  });
+  
